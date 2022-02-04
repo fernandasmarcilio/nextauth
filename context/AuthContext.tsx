80b53acfb35ec1d45f9bc23780fc96262ea7989b
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-04 18:44:17 -0300

    Create signIn

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,31 +1,71 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import Router from "next/router";
+import { setCookie } from "nookies";
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
   password: string;
-}
+};
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
-}
+  user: User;
+};
 
 type AuthProviderProps = {
   children: ReactNode;
-}
+};
 
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>(null);
+  const isAuthenticated = !!user;
 
   async function signIn({ email, password }: SignInCredentials) {
-    console.log( email, password );
-  } 
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { permissions, roles, token, refreshToken } = response.data;
+
+      // sessionStorage -> Dura somente dureante a sessão do usuárioo, se o usuario fechar o navegador e voltar não vai mais ter os dados.
+      // localStorage -> Dura mesmo reiniciando o navegador. No lado do servidor do Next, não tem acesso ao localStorage
+      // cookies -> Pode ser acessado tanto pelo lado do browser quanto do servidor
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: "/",
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <AuthContext.Provider value={{isAuthenticated, signIn}}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
