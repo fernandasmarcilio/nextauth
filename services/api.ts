4ea509399cb51fb475805791775e58dd6aaa06aa
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-07 11:18:43 -0300

    Add request queue with axios

--- services/api.ts
+++ services/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from 'nookies';
 
 let cookies = parseCookies();
+let isRefreshing = false;
+let failedRequestsQueue = [];
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
@@ -18,21 +20,51 @@ api.interceptors.response.use(
         cookies = parseCookies();
         const { 'nextauth.refreshToken': refreshToken } = cookies 
 
-        api.post('/refresh', { refreshToken }).then(response => {
-          const { token, refreshToken: newRefreshToken } = response.data;
+        const originalConfig = responseError.config;
 
-          setCookie(undefined, "nextauth.token", token, {
-            maxAge: 60 * 60 * 24 * 30, // 30 days
-            path: "/",
-          });
+        if(!isRefreshing) {
+          isRefreshing = true;
 
-          setCookie(undefined, "nextauth.refreshToken", newRefreshToken, {
-            maxAge: 60 * 60 * 24 * 30, // 30 days
-            path: "/",
-          });
+          api.post('/refresh', { refreshToken })
+          .then(response => {
+            const { token, refreshToken: newRefreshToken } = response.data;
+  
+            setCookie(undefined, "nextauth.token", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: "/",
+            });
+  
+            setCookie(undefined, "nextauth.refreshToken", newRefreshToken, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: "/",
+            });
+  
+            api.defaults.headers['Authorization'] = `Beaser ${token}`
 
-          api.defaults.headers['Authorization'] = `Beaser ${token}`
-          
+
+            failedRequestsQueue.forEach(request => request.resolve(token))
+            failedRequestsQueue = [];
+          })
+          .catch(() => {
+            failedRequestsQueue.forEach(request => request.reject())
+            failedRequestsQueue = [];
+          })
+          .finally(() => {
+            isRefreshing = false;
+          })
+        }
+
+        // axios não permite async então uma das formas é declarar a promise e usar o resolve e/ou reject para aguardar o que quer ser executado
+        return new Promise((resolve, reject) => {
+          failedRequestsQueue.push({
+            resolve: (token: string) => {
+              originalConfig.headers['Authorization'] = `Beaser ${token}`
+              resolve(api(originalConfig))
+            },
+            reject: (error: AxiosError) => {
+              reject(error )
+            },
+          })
         })
       } else {
          
