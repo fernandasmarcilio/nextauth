4aa9c93f3dbabb83b3c52eca0427f0983125c156
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-08 17:45:28 -0300

    Create higher-order functions to check guest on SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import { GetServerSideProps } from "next";
-import { parseCookies } from "nookies";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -31,19 +30,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-
-  if(cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-}
+})
