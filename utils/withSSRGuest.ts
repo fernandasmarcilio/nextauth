f5a93e582da74279f6526b3d89059f82427fff9a
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-08 17:46:37 -0300

    Add comments

--- utils/withSSRGuest.ts
+++ utils/withSSRGuest.ts
@@ -1,6 +1,7 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { parseCookies } from "nookies";
 
+// Higher-order functions -> Função que retorna uma função ou recebe como parametro uma função
 export function withSSRGuest<P>(fn: GetServerSideProps<P>): GetServerSideProps {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
