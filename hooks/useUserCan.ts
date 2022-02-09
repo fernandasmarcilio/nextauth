495c6c441adfa483d50f14cfbefe9f43ba2a2989
fernandasmarcilio <fernandas.marcilio@hotmail.com>
2022-02-09 16:19:06 -0300

    Add permissions and roles hook

---
+++ hooks/useUserCan.ts
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext"
+
+type UseUserCanParams = {
+  permissions?: string[];
+  roles?: string[];
+}
+
+export function useUserCan({ permissions, roles }: UseUserCanParams) {
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  if(!isAuthenticated) {
+    return false;
+  }
+
+  
+  if(permissions?.length > 0) {
+    const hasAllPermissions = permissions.every( permission => {
+      return user.permissions.includes(permission);
+    })
+    
+    if(!hasAllPermissions) {
+      return false
+    }
+
+  }
+
+  if(roles?.length > 0) {
+    const hasAllRoles = roles.some( role => {
+      return user.roles.includes(role);
+    })
+    
+    if(!hasAllRoles) {
+      return false
+    }
+
+  }
+
+  return true
+
+}
